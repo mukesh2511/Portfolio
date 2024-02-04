cfa7f4b9ede95362c11671e4033359341b23bd65
Mukesh <mukesh@meizoinfotech.com>
2024-02-05 01:08:30 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer/Footer.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Portfolio from "./pages/Portfolio/Portfolio.jsx";
+import Resume from "./pages/Resume/Resume.jsx";
+import Contact from "./pages/Contact/Contact.jsx";
+import "./App.css";
+import SideBar from "./components/SIdeBar/SIdeBar.jsx";
+
+const App = () => {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <div className="sidebar">
+          <SideBar />
+        </div>
+        <div className="others">
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/portfolio",
+          element: <Portfolio />,
+        },
+        {
+          path: "/resume",
+          element: <Resume />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SIdeBar/SideBar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SideBar.scss";
+
+const SIdeBar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default SIdeBar;
